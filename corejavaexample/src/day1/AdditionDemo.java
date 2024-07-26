56af062e36270bb683f9cc7f785a5a9b88ac1b89
vishali <vishali@DESKTOP-C79LMQU>
2024-07-26 16:08:33 -0700

    This is my day 1 java project

---
+++ corejavaexample/src/day1/AdditionDemo.java
@@ -0,0 +1,23 @@
+package day1;
+
+import java.util.Scanner;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s = new Scanner(System.in);
+		a = s.nextInt();
+		b = s.nextInt();
+		
+		s.nextLine();
+		
+		System.out.println("Enter the Trainer Nme:");
+		String name;
+		name = s.nextLine();
+		
+		c = a+b;
+		System.out.println("Addition of a ="+a+" and b="+b+" is "+c);
+		System.out.println("Tech Trainer:"+name);
+	}
+}
